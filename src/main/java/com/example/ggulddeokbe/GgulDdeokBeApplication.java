dc7d2114e461b63b9d61a51c8d536f682e65a526
민수아 <Minsua0503@gmail.com>
2026-03-26 17:22:45 +0900

    init

---
+++ src/main/java/com/example/ggulddeokbe/GgulDdeokBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ggulddeokbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgulDdeokBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgulDdeokBeApplication.class, args);
+    }
+
+}
