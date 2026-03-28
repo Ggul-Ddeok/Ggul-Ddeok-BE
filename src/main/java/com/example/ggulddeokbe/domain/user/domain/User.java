19afed7d596268e60bab3f1d36905f2f44fb1cb9
민수아 <Minsua0503@gmail.com>
2026-03-28 10:14:28 +0900

    feat: add user entity, repository

---
+++ src/main/java/com/example/ggulddeokbe/domain/user/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.ggulddeokbe.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+}
