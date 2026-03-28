5320fc2398d549714ed0e0068acee89d3fc9a991
민수아 <Minsua0503@gmail.com>
2026-03-28 15:29:37 +0900

    feat: AuthDetailsService

---
+++ src/main/java/com/example/ggulddeokbe/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.ggulddeokbe.global.security.auth;
+
+import com.example.ggulddeokbe.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+            .map(AuthDetails::new)
+            .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
