da95c57ec263607f8536815d561caace902ad13c
민수아 <Minsua0503@gmail.com>
2026-03-28 15:28:46 +0900

    feat: add @Getter

--- src/main/java/com/example/ggulddeokbe/domain/user/domain/User.java
+++ src/main/java/com/example/ggulddeokbe/domain/user/domain/User.java
@@ -1,13 +1,11 @@
 package com.example.ggulddeokbe.domain.user.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Builder
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
