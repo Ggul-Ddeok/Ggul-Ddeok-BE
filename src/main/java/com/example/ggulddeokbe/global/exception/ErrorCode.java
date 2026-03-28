533c95efb18dcf63fcf673182e959690b75b8190
민수아 <Minsua0503@gmail.com>
2026-03-28 15:29:07 +0900

    feat: oauth error code

--- src/main/java/com/example/ggulddeokbe/global/exception/ErrorCode.java
+++ src/main/java/com/example/ggulddeokbe/global/exception/ErrorCode.java
@@ -19,7 +19,13 @@ public enum ErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
 
     // User
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
+    // OAuth
+    KAKAO_ACCOUNT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "카카오 계정 정보를 찾을 수 없습니다."),
+    KAKAO_PROFILE_NOT_FOUND(HttpStatus.UNAUTHORIZED, "카카오 프로필 정보를 찾을 수 없습니다."),
+    OAUTH_EMAIL_NOT_FOUND(HttpStatus.UNAUTHORIZED, "OAuth 이메일 정보를 찾을 수 없습니다."),
+    OAUTH_ACCOUNT_ID_NOT_FOUND(HttpStatus.UNAUTHORIZED, "OAuth 계정 ID를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
