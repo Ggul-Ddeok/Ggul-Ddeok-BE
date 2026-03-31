80a0f6f9df1ee8134fd3a7bee3e01628e30069fc
민수아 <Minsua0503@gmail.com>
2026-04-01 08:41:34 +0900

    feat: UserFacade

---
+++ src/main/java/com/example/ggulddeokbe/domain/user/facade/UserFacade.java
@@ -0,0 +1,27 @@
+package com.example.ggulddeokbe.domain.user.facade;
+
+import com.example.ggulddeokbe.domain.user.domain.User;
+import com.example.ggulddeokbe.domain.user.domain.repository.UserRepository;
+import com.example.ggulddeokbe.global.exception.BaseException;
+import com.example.ggulddeokbe.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new BaseException(ErrorCode.UNAUTHORIZED);
+        }
+        String email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+    }
+}
