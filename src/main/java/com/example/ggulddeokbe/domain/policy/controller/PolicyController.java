015b97bd494d8710b3b93aba8da15d10c3ea7cf6
민수아 <Minsua0503@gmail.com>
2026-04-12 17:23:58 +0900

    feat: PolicyController

---
+++ src/main/java/com/example/ggulddeokbe/domain/policy/controller/PolicyController.java
@@ -0,0 +1,35 @@
+package com.example.ggulddeokbe.domain.policy.controller;
+
+import com.example.ggulddeokbe.domain.policy.domain.Region;
+import com.example.ggulddeokbe.domain.policy.dto.PolicyDetailResponse;
+import com.example.ggulddeokbe.domain.policy.dto.PolicyListResponse;
+import com.example.ggulddeokbe.domain.policy.service.PolicyQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ggulddeok/policies")
+@RequiredArgsConstructor
+public class PolicyController {
+
+    private final PolicyQueryService policyQueryService;
+
+    @GetMapping
+    public ResponseEntity<PolicyListResponse> getPolicies(
+            @RequestParam("region") Region region
+    ) {
+        return ResponseEntity.ok(policyQueryService.getPolicies(region));
+    }
+
+    @GetMapping("/{plcyNo}")
+    public ResponseEntity<PolicyDetailResponse> getPolicyDetail(
+            @PathVariable("plcyNo") String plcyNo
+    ) {
+        return ResponseEntity.ok(policyQueryService.getPolicyDetail(plcyNo));
+    }
+}
